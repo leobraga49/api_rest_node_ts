9bdbfed58df15ef710e6045f8567c3393958e2bc
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-25 09:40:11 -0300

    Created method getAll

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
     return res.send('Hello, World!');
 });
 
+router.get('/cities', CitiesController.getAllValidation, CitiesController.getAll);
 router.post('/cities', CitiesController.createValidation, CitiesController.create);
 
 export { router };
